eebcd5f0115af90afd74cfc3ec0a257624bc30b3
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-05-04 14:07:33 -0700

    merge class added

--- DAY4/Sorting-algos/Mergesort.java
+++ DAY4/Sorting-algos/Mergesort.java
@@ -15,5 +15,42 @@ public class Mergesort {
 		ob.sort(array, 0, array.length - 1);
 		print(array);
 	}
+	public static void merge(int array[], int big, int mid, int end) {
+		int l = mid - big + 1;
+		int r = end - mid;
+
+		int LeftArray[] = new int[l];
+		int RightArray[] = new int[r];
+
+		for (int i = 0; i < l; ++i)
+			LeftArray[i] = array[big + i];
+
+		for (int j = 0; j < r; ++j)
+			RightArray[j] = array[mid + 1 + j];
+
+		int i = 0, j = 0;
+		int k = big;
+		while (i < l && j < r) {
+			if (LeftArray[i] <= RightArray[j]) {
+				array[k] = LeftArray[i];
+				i++;
+			} else {
+				array[k] = RightArray[j];
+				j++;
+			}
+			k++;
+		}
+		while (i < l) {
+			array[k] = LeftArray[i];
+			i++;
+			k++;
+		}
+
+		while (j < r) {
+			array[k] = RightArray[j];
+			j++;
+			k++;
+		}
+	}
 }
 
