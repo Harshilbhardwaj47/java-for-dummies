05beed5f98adcce294138229abf1c84a42410472
Harshil bhardwaj <57087065+Harshilbhardwaj47@users.noreply.github.com>
2020-05-05 01:14:57 -0700

    Update File8.java

--- DAY5/Fileoperations/Fileconvertion/File8.java
+++ DAY5/Fileoperations/Fileconvertion/File8.java
@@ -1,11 +1,11 @@
-package DAY6;
+package DAY5;
 
 import java.io.File;
 import java.io.IOException;
 
 public class File8 {
 	public static void main(String[] args) {
-		File myfile = new File("G:\\HACKERRANK\\JAVAFORDUMMIES\\src\\DAY6\\newfile.txt");
+		File myfile = new File("G:\\HACKERRANK\\JAVAFORDUMMIES\\src\\DAY5\\newfile.txt");
 		if (myfile.canWrite()) {
 			System.out.println("File is writable.");
 		} else {
