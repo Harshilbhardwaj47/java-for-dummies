f127761cc4504aa87fd2e5409588cfff07f3cabf
Harshil bhardwaj <57087065+Harshilbhardwaj47@users.noreply.github.com>
2020-05-05 01:13:35 -0700

    Update File11.java

--- DAY5/Fileoperations/FileRecord/File11.java
+++ DAY5/Fileoperations/FileRecord/File11.java
@@ -1,4 +1,4 @@
-package DAY6;
+package DAY5;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -6,7 +6,7 @@ import java.text.SimpleDateFormat;
 public class File11 {
 	public static void main(String[] args) {
 		// Specify the file path and name
-		File file = new File("G:\\HACKERRANK\\JAVAFORDUMMIES\\src\\DAY6\\newfile.txt");
+		File file = new File("G:\\HACKERRANK\\JAVAFORDUMMIES\\src\\DAY5\\newfile.txt");
 		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
 		System.out.println("Last Modified Date: " + sdf.format(file.lastModified()));
 	}
