1e35d3dacad9a63f3f29fd0a9d5b2d5a260a33ca
Pranit5895 <guptapranit9@gmail.com>
2020-05-09 00:42:45 -0700

    Definition of compareTo() function in strings

---
+++ DAY3/Task/Q1.java
@@ -0,0 +1,3 @@
+The Java String compareTo() method is used for comparing two strings lexicographically. Each character of both the strings is converted into a Unicode value for comparison. If both the strings are equal then this method returns 0 else it returns positive or negative value.
+
+
