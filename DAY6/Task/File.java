5030b4b3becf028bbc73cd529652487fa7e34829
Pranit5895 <guptapranit9@gmail.com>
2020-05-10 23:45:51 -0700

    Try catch block to check file exists or not

--- DAY6/Task/File.java
+++ DAY6/Task/File.java
@@ -30,4 +30,26 @@ public class Files {
 		File file = new File("D:\\JAVA\\newfile.txt");
 		BufferedInputStream bis = null;
 		FileInputStream fis = null;
+try {
+			// FileInputStream to read the file
+			fis = new FileInputStream(file);
 
+			/*
+			 * Passed the FileInputStream to BufferedInputStream For Fast read using the
+			 * buffer array.
+			 */
+			bis = new BufferedInputStream(fis);
+
+			/*
+			 * available() method of BufferedInputStream returns 0 when there are no more
+			 * bytes present in the file to be read
+			 */
+			while (bis.available() > 0) {
+				System.out.print((char) bis.read());
+			}
+
+		} catch (FileNotFoundException fnfe) {
+			System.out.println("The specified file not found" + fnfe);
+		} catch (IOException ioe) {
+			System.out.println("I/O Exception: " + ioe);
+		}
