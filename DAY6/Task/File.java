51f685f217f4be3e3357ff6a694246f8c58f3a68
Pranit5895 <guptapranit9@gmail.com>
2020-05-10 23:47:44 -0700

    After password match converted file to write mode

--- DAY6/Task/File.java
+++ DAY6/Task/File.java
@@ -70,3 +70,32 @@ str Pass2= in.nextInt();
 			String mycontent = in.nextLine();
 			// Specify the file name and path here
 			File file = new File("G:\\HACKERRANK\\JAVAFORDUMMIES\\src\\DAY5\\newfile.txt");
+			/*
+			 * This logic will make sure that the file gets created if it is not present at
+			 * the specified location
+			 */
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file);
+			bw = new BufferedWriter(fw);
+			bw.write(mycontent);
+			System.out.println("File written Successfully");
+
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			try {
+				if (bw != null)
+					bw.close();
+			} catch (Exception ex) {
+				System.out.println("Error in closing the BufferedWriter" + ex);
+			}
+		}
+	}
+	else
+	{ System.out.println("Wrong Password");
+	}
+    }
+}
