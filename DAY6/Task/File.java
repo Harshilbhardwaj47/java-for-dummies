54fbe35ae621882aeb8634375850ddf638d57afa
Pranit5895 <guptapranit9@gmail.com>
2020-05-10 23:41:53 -0700

    Class file started with text input from user to store in file

--- DAY6/Task/File.java
+++ DAY6/Task/File.java
@@ -5,4 +5,11 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.util.Scanner;
+public class Files {
 
+    public static void main(String args[]){
+
+        System.out.print("Enter Text: ");
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+        FileWriter fWriter = null;
