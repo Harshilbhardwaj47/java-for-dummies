cbe05bdd87579c7583db54931d783cbe2f6f0873
Pranit5895 <guptapranit9@gmail.com>
2020-05-12 04:57:32 -0700

    Derived class constructor declared

--- DAY7/Task/Inheritance.java
+++ DAY7/Task/Inheritance.java
@@ -40,4 +40,11 @@ class MountainBike extends Bicycle
 
     // the MountainBike subclass adds one more field
     public int seatHeight;
-
+// the MountainBike subclass has one constructor
+    public MountainBike(int gear,int speed,
+                        int startHeight)
+    {
+        // invoking base-class(Bicycle) constructor
+        super(gear, speed);
+        seatHeight = startHeight;
+    }
