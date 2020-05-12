b9b702f6c4ebc544e4020ffa886fae917996eb9f
Pranit5895 <guptapranit9@gmail.com>
2020-05-12 05:05:43 -0700

    Driver class for function calling hence interface implementation completed

--- DAY8/Task/Interface1.java
+++ DAY8/Task/Interface1.java
@@ -20,3 +20,11 @@ class TestClass implements In1
     {
         System.out.println("Geek");
     }
+    // Driver Code
+    public static void main (String[] args)
+    {
+        TestClass t = new TestClass();
+        t.display();
+        System.out.println(a);
+    }
+}
