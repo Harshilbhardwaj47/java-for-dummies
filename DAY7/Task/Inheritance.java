e6455368edfd78c914dcbb7e0d7280e19ae6100d
Pranit5895 <guptapranit9@gmail.com>
2020-05-12 04:59:23 -0700

    Driver class completed hence inheritance implementation done

--- DAY7/Task/Inheritance.java
+++ DAY7/Task/Inheritance.java
@@ -65,3 +65,14 @@ class MountainBike extends Bicycle
     }
 
 }
+// driver class
+public class Test
+{
+    public static void main(String args[])
+    {
+
+        MountainBike mb = new MountainBike(3, 100, 25);
+        System.out.println(mb.toString());
+
+    }
+}
