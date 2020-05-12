0515d70d468693261e6dfa7f217b701ab4afef5e
Pranit5895 <guptapranit9@gmail.com>
2020-05-12 04:49:52 -0700

    Base class started

---
+++ DAY7/Task/Inheritance.cpp
@@ -0,0 +1,10 @@
+//Java program to illustrate the  
+// concept of inheritance 
+  
+// base class 
+class Bicycle  
+{ 
+    // the Bicycle class has two fields 
+    public int gear; 
+    public int speed; 
+          
