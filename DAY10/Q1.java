f475c0f42c93eb0a80b798694a43240b56e2bb46
Pranit5895 <guptapranit9@gmail.com>
2020-05-14 06:13:01 -0700

    Overloading functions declared

--- DAY10/Q1.java
+++ DAY10/Q1.java
@@ -3,3 +3,12 @@ class Rectangle{         //OVERLOADING CLASS
   public static void printArea(int x,int y){
 	  System.out.println(x*y);
 	   }
+  public static void printArea(int x){       //overloading function 1
+    System.out.println(x*x);
+  }
+   public static void printArea(int x,double y){  //overloading function 2
+    System.out.println(x*y);
+  }
+  public static void printArea(double x){   //overloading function 3
+    System.out.println(x*x);
+    }
