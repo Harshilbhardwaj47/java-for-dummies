68cc2f8e174234e4a9b33166bb9f11f0cdf70a94
Pranit5895 <guptapranit9@gmail.com>
2020-05-14 06:19:05 -0700

    main() function declared for function calling

--- DAY10/Q1.java
+++ DAY10/Q1.java
@@ -12,3 +12,10 @@ class Rectangle{         //OVERLOADING CLASS
   public static void printArea(double x){   //overloading function 3
     System.out.println(x*x);
     }
+   public static void main(String[] args){   //main fucntion for implementation
+    printArea(2,4);
+    printArea(2,5.1);
+    printArea(10);
+    printArea(2.3);
+}
+}
