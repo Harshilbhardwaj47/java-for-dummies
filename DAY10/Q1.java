4b2f0772f2e29933a9e17b7553c7c07a421d9007
Pranit5895 <guptapranit9@gmail.com>
2020-05-14 06:22:02 -0700

    Inherited classes declared

--- DAY10/Q1.java
+++ DAY10/Q1.java
@@ -24,3 +24,18 @@ class Animals{        //CLASS FOR OVERRIDING
     System.out.println("This is parent class.");
   }
 }
+class Dogs extends Animals{    //inherited class 1
+  public void sound(){
+    System.out.println("Dogs bark");
+  }
+}
+class Cats extends Animals{    //inherited class 2
+  public void sound(){
+    System.out.println("Cats meow");
+  }
+}
+class Monkeys extends Animals{    //inherited class 3
+  public void sound(){
+    System.out.println("Monkeys whoop.");
+  }
+}
