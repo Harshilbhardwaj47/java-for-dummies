f455a19ca9d1864a564bc908d97436a005232f1b
Pranit5895 <guptapranit9@gmail.com>
2020-05-18 01:07:13 -0700

    Try block for checking conditions

--- DAY14/Solution/Solution.java
+++ DAY14/Solution/Solution.java
@@ -3,4 +3,14 @@ public class String{
 	public static void main(String[] args) {
 Scanner input =  new Scanner(System.in);
 String name = "";
+System.out.println("Please enter your name: ");
+name = input.nextLine();
+
+name= name.toLowerCase();
+
+try{
+    if(!name.find()){ // If string contains any number/symbols etc...
+        throw new Exception("Not a perfect String");
+    }
+
 
